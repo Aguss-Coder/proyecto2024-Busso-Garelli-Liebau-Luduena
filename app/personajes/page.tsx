04e9365687045740ce20fed2ce50d95430279f8b
Aguss-Coder <luduenagustin615@gmail.com>
2024-05-08 15:55:18 -0300

    fix build bug

--- app/personajes/page.tsx
+++ app/personajes/page.tsx
@@ -2,34 +2,39 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState, useMemo } from "react";
-import data from "@/data/personajes.json";
+import { useState, useMemo, useEffect } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-type Personajes = {
+type Characters = {
   name: string;
   image: string;
 }
 
-function getCharacters(): Personajes[] {
+function getCharacters(): Characters[] {
   const storedCharacters = localStorage.getItem("characters");
 
-  return !storedCharacters ? [] : JSON.parse(storedCharacters) as Personajes[]
+  return !storedCharacters ? [] : JSON.parse(storedCharacters) as Characters[]
 }
 
 export default function Page() {
-  const personajes: Personajes[] = getCharacters();
+  const [storedCharacters, setStoredCharacters] = useState<Characters[]>([]);
 
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   const paginatedData = useMemo(() => {
-    if (!personajes || personajes.length === 0) return [];
+    if (!storedCharacters || storedCharacters.length === 0) return [];
 
     const start = (currentPage - 1) * 3;
     const end = start + 3;
 
-    return personajes.slice(start, end);
-  }, [personajes, currentPage]);
+    return storedCharacters.slice(start, end);
+  }, [storedCharacters, currentPage]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setStoredCharacters(getCharacters());
+    }
+  }, [])
 
   return (
     <main>
@@ -42,21 +47,21 @@ export default function Page() {
           <button className="h-fit disabled:opacity-25" onClick={ () => (setCurrentPage(currentPage - 1)) } disabled={ currentPage === 1}>
             <FaArrowLeft size={24} />
           </button>
-          { paginatedData.length === 0 ? <h3 className="px-8 py-4 text-3xl bg-principal-1">No tenes personajes creados</h3> : paginatedData.map((personaje) => (
-            <div key={personaje.name} className="bg-card h-96 w-64 flex flex-col items-center rounded-lg">
-                <h3 className="py-4 text-center text-principal-dark text-2xl">{personaje.name}</h3>
-                <Image src={personaje.image} width={170} height={170} alt={personaje.name} className="rounded-lg" />
+          { paginatedData.length === 0 ? <h3 className="px-8 py-4 text-3xl bg-principal-1">No tenes personajes creados</h3> : paginatedData.map((character) => (
+            <div key={character.name} className="bg-card h-96 w-64 flex flex-col items-center rounded-lg">
+                <h3 className="py-4 text-center text-principal-dark text-2xl">{character.name}</h3>
+                <Image src={character.image} width={170} height={170} alt={character.name} className="rounded-lg" />
                 <button className="bg-principal-1 text-3xl py-0.5 px-3">
                   <Link href="">Editar</Link>
                 </button>
             </div>
           )) }
-          <button className="h-fit disabled:opacity-25" onClick={ () => (setCurrentPage(currentPage + 1)) } disabled={ currentPage >= Math.round(personajes.length / 3) }>
+          <button className="h-fit disabled:opacity-25" onClick={ () => (setCurrentPage(currentPage + 1)) } disabled={ currentPage >= Math.round(storedCharacters.length / 3) }>
             <FaArrowRight size={24} />
           </button>
         </section>
         <section>
-          <p className="text-2xl text-center">Pag: { currentPage }/{ Math.round((personajes.length/3)) || 1 }</p>
+          <p className="text-2xl text-center">Pag: { currentPage }/{ Math.round((storedCharacters.length/3)) || 1 }</p>
           <button className={`bg-principal-1 py-2 px-5`}>
             <Link href="/personajes/crear_personajes">
               Crear Personaje
