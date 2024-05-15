f3d9fc1b3f33aa7bef40d5f262b29c66ec7cc353
ValentinGarelli <2309846@ucc.edu.ar>
2024-05-14 23:38:59 -0300

    fix styling manuals

--- app/manuales/page.tsx
+++ app/manuales/page.tsx
@@ -7,6 +7,7 @@ export default function Page() {
   return (
     <main className='h-screen'>
       <div className='content-background flex justify-center items-center flex-wrap'>
+        <h1 className='text-4xl absolute top-8'>Manuales del Juego</h1>
         <Link
           href='/'
           className='fixed p-2 left-8 top-4 w-fit h-fit bg-principal-1 rounded-full'
@@ -15,27 +16,27 @@ export default function Page() {
         </Link>
 
         <div className='w-1/8'>
-          <h1 className='mt-8 text-4xl text-center'>Manuales</h1>
+          <h3 className='mt-8 text-2xl text-center'>Manuales</h3>
           <section className='flex justify-center items-center'>
-            <button className='bg-principal-2 py-4 px-12'>
+            <button className='bg-principal-2 py-2 px-6'>
               <a href='/manualNovaTerra.pdf' download='manualNovaTerra.pdf'>
                 Descargar
               </a>
             </button>
           </section>
 
-          <h1 className='mt-8 text-4xl text-center'>Clases jugables</h1>
+          <h3 className='mt-8 text-2xl text-center'>Clases jugables</h3>
           <section className='flex justify-center items-center'>
-            <button  className='bg-principal-3 py-4 px-12'>
+            <button  className='bg-principal-3 py-2 px-6'>
               <a href='/Clases jugables-1.pdf' download='Clases jugables-1.pdf'>
                 Descargar
               </a>
             </button>
           </section>
 
-          <h1 className='mt-8 text-4xl text-center'>Habilidades comunes</h1>
+          <h3 className='mt-8 text-2xl text-center'>Habilidades comunes</h3>
           <section className='flex justify-center items-center'>
-            <button className='bg-principal-2 py-4 px-12'>
+            <button className='bg-principal-2 py-2 px-6'>
               <a href='/Habilidades comunes-1.pdf' download='Habilidades comunes-1.pdf'>
                 Descargar
               </a>
@@ -43,27 +44,27 @@ export default function Page() {
           </section>
         </div>
         <div className='w-1/8'>
-          <h1 className='mt-8 text-4xl text-center'>Dotes comunes</h1>
+          <h3 className='mt-8 text-2xl text-center'>Dotes comunes</h3>
           <section className='flex justify-center items-center'>
-            <button className='bg-principal-3 py-4 px-12'>
+            <button className='bg-principal-3 py-2 px-6'>
               <a href='/Dotes comunes-1.pdf' download='Dotes comunes-1.pdf'>
                 Descargar
               </a>
             </button>
           </section>
 
-          <h1 className='mt-8 text-4xl text-center'>Estadisticas</h1>
+          <h3 className='mt-8 text-2xl text-center'>Estadisticas</h3>
           <section className='flex justify-center items-center'>
-            <button className='bg-principal-2 py-4 px-12'>
+            <button className='bg-principal-2 py-2 px-6'>
               <a href='/Estadisticas.pdf' download='Estadisticas.pdf'>
                 Descargar
               </a>
             </button>
           </section>
 
-          <h1 className='mt-8 text-4xl text-center'>Idiomas</h1>
+          <h3 className='mt-8 text-2xl text-center'>Idiomas</h3>
           <section className='flex justify-center items-center'>
-            <button className='bg-principal-3 py-4 px-12'>
+            <button className='bg-principal-3 py-2 px-6'>
               <a href='/Idiomas-1.pdf' download='Idiomas-1.pdf'>
                 Descargar
               </a>
