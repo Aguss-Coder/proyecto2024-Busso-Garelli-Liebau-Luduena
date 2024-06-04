3a7f34d6600a720f47dcb64433f360761f5382fb
Aguss-Coder <luduenagustin615@gmail.com>
2024-06-04 19:11:24 -0300

    fix(animation): fix animation code

--- app/animacion/page.tsx
+++ app/animacion/page.tsx
@@ -1,62 +1,65 @@
 'use client'
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export default function page() {
+const canvasAnimation: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const colores = ['#f00', '#00f', '#0f0', '#ff0', '#0ff', '#f0f' , '#f00ff', '#000',];
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas === null) throw new Error('Canvas no encontrado');
+    const ctx = canvas.getContext('2d');
+
+    const radius = 10;
+
+    let x = canvas.width / 3;
+    let y = canvas.height / 3;
+
+    let dx = 0.8;
+    let dy = 0.8;
+
+    let color = Math.floor(Math.random() * colores.length);
+
+    const drawBall = () => {
+      if (!ctx) return;
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fillStyle = colores[color];
+      ctx.fill();
+    };
+
+    const updateBall = () => {
+      x += dx;
+      y += dy;
+
+      if (y + radius > canvas.height || y - radius < 0) {
+        dy *= -1;
+        color = Math.floor(Math.random() * colores.length);
+      }
+
+      if (x + radius > canvas.width || x - radius < 0) {
+        dx *= -1;
+        color = Math.floor(Math.random() * colores.length);
+      }
+    };
+
+    const animate = () => {
+      if (!ctx) return;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawBall();
+      updateBall();
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, []);
+
   return (
     <div className="m-0 overflow-hidden w-screen h-screen">
-      <canvas id="lienzo" className="bg-principal-light w-screen h-screen"></canvas>
+      <canvas ref={canvasRef} className="bg-principal-light w-screen h-screen"></canvas>
     </div>
-  );
+  )
 }
 
-const canvas = document.getElementById('lienzo') as HTMLCanvasElement;
-if (canvas === null) throw new Error('Canvas no encontrado');
-const ctx = canvas.getContext('2d');
-
-const colores = ['#f00', '#00f', '#0f0', '#ff0', '#0ff', '#f0f' , '#f00ff', '#000',];
-
-const width = canvas.width;
-const height = canvas.height;
-
-const ballRadius = 10;
-
-let x = width / 3;
-let y = height / 3;
-
-let dx = 0.8;
-let dy = 0.8;
-
-let color = Math.floor(Math.random() * colores.length);
-
-function drawBall() {
-  if (!ctx) return;
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = colores[color];
-  ctx.fill();
-}
-
-function updateBall() {
-  x += dx;
-  y += dy;
-
-  if (y + ballRadius > height || y - ballRadius < 0) {
-    dy *= -1;
-    color = Math.floor(Math.random() * colores.length);
-  }
-
-  if (x + ballRadius > width || x - ballRadius < 0) {
-    dx *= -1;
-    color = Math.floor(Math.random() * colores.length);
-  }
-}
-
-function animate() {
-  if (!ctx) return;
-  ctx.clearRect(0, 0, width, height);
-  drawBall();
-  updateBall();
-  requestAnimationFrame(animate);
-}
-
-animate();
+export default canvasAnimation;
